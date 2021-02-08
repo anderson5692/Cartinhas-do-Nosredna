c46d694bc9cac8acb82185e4b000dfc451e8db4f
anderson5692 <blueoystercult1982@gmail.com>
2021-02-07 23:14:11 -0300

    add button

--- projeto1/Scripts/cartinha/views.py
+++ projeto1/Scripts/cartinha/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    #dicionario_contexto = {'msgnegrito':""}
+    #d1 = {'':""}
     return render(request, 'django/index.html')
+    return render(request, 'django/page1.html')
+    
--- projeto1/Scripts/django_project/urls.py
+++ projeto1/Scripts/django_project/urls.py
@@ -22,5 +22,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', views.home, name='home')
+    path('home/', views.home, name='home')   
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
